57c6c55fe660ad29436cd12d0890d15441765273
cuasso <facu.cuassolo.95@gmail.com>
2020-09-10 20:28:16 -0300

    se agrega logia de agregar/quitar ingredientes

--- burguer-builder/src/containers/BurgerBuilder/index.jsx
+++ burguer-builder/src/containers/BurgerBuilder/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Burger from '../../components/Burger'
-import Aux from '../../hoc/Helps'
+import Aux from '../../hoc/Aux'
 import BuildControls from '../../components/Burger/BuildControls'
 
 const INGREDIENT_PRICES = {
@@ -16,7 +16,7 @@ class BurgerBuilder extends Component {
             salad: 1,
             bacon: 1,
             cheese: 2,
-            meet: 2
+            meat: 2
         },
         totalPrice: 4
     }
@@ -28,18 +28,51 @@ class BurgerBuilder extends Component {
             ...this.state.ingredients
         }
         updatedIngredients[type] = updatedCount
-
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
     }
 
     removeIngredientHandler = (type) => {
-
+        const oldCount = this.state.ingredients[type]
+        if (oldCount <= 0) {
+            return
+        }
+        const updatedCount = oldCount - 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+        const priceDeduction = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
     }
 
     render() {
+        const disableInfo = {
+            ...this.state.ingredients
+        }
+
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}
+                />
             </Aux>
 
         )
