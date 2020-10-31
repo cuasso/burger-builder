b0ba59950af23ebfc18c9d9b5fe64cb9c4395d24
cuasso <facu.cuassolo.95@gmail.com>
2020-10-31 12:52:54 -0300

    Se agrega logica de checkout y order

--- burguer-builder/src/containers/BurgerBuilder/index.jsx
+++ burguer-builder/src/containers/BurgerBuilder/index.jsx
@@ -96,30 +96,45 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinuehandler = () => {
-        /* this.setState({ loading: true })
-         const order = {
-             ingredients: this.state.ingredients,
-             price: this.state.totalPrice,
-             customer: {
-                 name: 'lafken',
-                 addres: {
-                     street: 'Echeverria 4661',
-                     zipCode: '1757',
-                     country: 'Argentina'
-                 },
-                 email: 'lafken@gmail.com'
-             },
-             deliveryMethod: 'fastest'
-         }
- 
-         axios.post('/orders.json', order)
-             .then(response => {
-                 this.setState({ loading: false, purchasing: false })
-             })
-             .catch(error => {
-                 this.setState({ loading: false, purchasing: false })
-             })*/
-        this.props.history.push('/checkout')
+        /*this.setState({ loading: true })
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'lafken',
+                addres: {
+                    street: 'Echeverria 4661',
+                    zipCode: '1757',
+                    country: 'Argentina'
+                },
+                email: 'lafken@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false, purchasing: false })
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false })
+            })*/
+
+        console.log(this.state.ingredients)
+        const queryParams = []
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '='
+                + encodeURIComponent(this.state.ingredients[i]))
+        }
+
+        queryParams.push('price=' + this.state.totalPrice)
+
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     }
 
     render() {
--- burguer-builder/src/containers/Checkout/index.jsx
+++ burguer-builder/src/containers/Checkout/index.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import CheckoutSummary from '../../components/Order/CheckoutSummary'
+import ContactData from '../Checkout/ContactData'
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 3,
-            bacon: 2
+        ingredients: null,
+        totalPrice: 0
+    }
+
+    componentWillMount() {
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        let price = 0
+        for (let param of query.entries()) {
+            if (param[0] === 'price') {
+                price = param[1]
+            } else {
+                ingredients[param[0]] = + param[1]
+            }
         }
+        this.setState({ ingredients: ingredients, totalPrice: price })
     }
 
     CheckoutCancelledHandler = () => this.props.history.goBack()
@@ -24,6 +36,8 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     onCheckoutCancelled={this.CheckoutCancelledHandler}
                     onCheckoutContinue={this.CheckoutContinueHandler} />
+                <Route path={this.props.match.path + '/contact-data'}
+                    render={(props) => (<ContactData ingredients={this.props.ingredients} price={this.state.totalPrice} {...props}/>)} />
             </div>
         )
     }
