24f7634ec5ca7b1cc348ba70dc41e272218c8e7e
cuasso <facu.cuassolo.95@gmail.com>
2020-11-07 01:05:57 -0300

    agrego cosas

--- burguer-builder/src/App.js
+++ burguer-builder/src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Layout from './containers/Layout'
-import BurgerBuilder from './containers/BurgerBuilder'
 import Checkout from './containers/Checkout'
+import BurgerBuilder from './containers/BurgerBuilder'
+import Orders from './containers/Orders'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Layout>
         <Switch >
           <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
           <Route path='/' exact component={BurgerBuilder} />
         </Switch>
       </Layout>
